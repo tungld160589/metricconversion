43845970ece10aaa125b616808c9aa90f4067912
tungld160589 <148056136+tungld160589@users.noreply.github.com>
2023-12-23 22:43:32 +0700

    cập nhật listbox

--- src/components/ConvertGraph.jsx
+++ src/components/ConvertGraph.jsx
@@ -7,10 +7,16 @@ const ConvertGraph = (props) => {
   const [leftList, setleftList] = useState();
   const [rightList, setRightList] = useState();
   const [listVisible, setListVisible] = useState(false);
+  const [listVisibleRight, setListVisibleRight] = useState(false);
+  const [selectOptionRight, setSelectOptionRight] = useState(null);
   const [selectedOption, setSelectedOption] = useState(null);
   const selectOption = (option) => {
     setSelectedOption(option);
     setListVisible(false);
+  };
+  const selectOptionR = (option) => {
+    setSelectOptionRight(option);
+    setListVisibleRight(false);
   };
   const toggleList = () => {
     setListVisible(!listVisible);
@@ -20,6 +26,18 @@ const ConvertGraph = (props) => {
     });
     setleftList(sortlist);
   };
+  const toggleListR = () => {
+    setListVisibleRight(!listVisibleRight);
+  };
+
+  const handleClickLeft = (option) => {
+    const updatedUnitList = leftList.filter((e) => {
+      let unitChoose = option.nameOfUnit.toLowerCase();
+
+      return e.nameOfUnit !== option.nameOfUnit;
+    });
+    setRightList(updatedUnitList);
+  };
   return (
     <div className="convertgraph-control">
       <div className="bg-group-convert-control">
@@ -27,23 +45,26 @@ const ConvertGraph = (props) => {
           <div>
             <div className="convert-left-control">
               <div className="txt-convert-left" onClick={toggleList}>
-                <span>{selectedOption || "Choose Unit Convert To:"}</span>
+                <span>{selectedOption || "Choose Unit Convert From:"}</span>
                 <div className={`arrow-icon ${listVisible ? "open" : ""}`}>
                   &#11167;
                 </div>
+                {listVisible && (
+                  <ul className="list-unit-convert">
+                    {leftList.map((option, index) => (
+                      <li
+                        key={index}
+                        onClick={() => {
+                          selectOption(option.nameOfUnit);
+                          handleClickLeft(option);
+                        }}
+                      >
+                        {option.nameOfUnit}
+                      </li>
+                    ))}
+                  </ul>
+                )}
               </div>
-              {listVisible && (
-                <ul className="list-container">
-                  {leftList.map((option, index) => (
-                    <li
-                      key={index}
-                      onClick={() => selectOption(option.nameOfUnit)}
-                    >
-                      {option.nameOfUnit}
-                    </li>
-                  ))}
-                </ul>
-              )}
             </div>
 
             <input
@@ -55,8 +76,25 @@ const ConvertGraph = (props) => {
         </div>
         <div className="bg-convert-right-control">
           <div className="convert-right-control">
-            <div className="txt-convert-right">
-              <p>Choose unit convert to</p> <p>&#11167;</p>
+            <div className="txt-convert-right" onClick={toggleListR}>
+              <span>{selectOptionRight || "Choose Unit Convert To:"}</span>
+              <div className={`arrow-icon ${listVisibleRight ? "open" : ""}`}>
+                &#11167;
+              </div>
+              {listVisibleRight && (
+                <ul className="list-unit-convert">
+                  {rightList.map((optionR, indexR) => (
+                    <li
+                      key={indexR}
+                      onClick={() => {
+                        selectOptionR(optionR.nameOfUnit);
+                      }}
+                    >
+                      {optionR.nameOfUnit}
+                    </li>
+                  ))}
+                </ul>
+              )}
             </div>
             <input
               className="output-right"
@@ -70,27 +108,6 @@ const ConvertGraph = (props) => {
         </div>
         <div className="convert-result"></div>
       </div>
-      {/* <div className="convert-left-control">
-        <div className="txt-convert-left">
-          <p>Choose unit convert</p>
-          <p>&#11167;</p>
-        </div>
-        <input
-          className="input-left"
-          type="text"
-          placeholder="gia tri convert"
-        />
-      </div>
-      <div className="convert-right-control">
-        <div className="txt-convert-right">
-          <p>Choose unit convert to</p> <p>&#11167;</p>
-        </div>
-        <input
-          className="output-right"
-          type="text"
-          placeholder="ket qua chuyển đổi"
-        />
-      </div> */}
     </div>
   );
 };
