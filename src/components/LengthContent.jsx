3a114f57f714024d94b58051b5b427f7213542f6
tungld160589 <148056136+tungld160589@users.noreply.github.com>
2023-12-28 22:56:35 +0700

    cập nhật Conent cho Volum và length

--- src/components/LengthContent.jsx
+++ src/components/LengthContent.jsx
@@ -5,38 +5,175 @@ const LengthContent = () => {
         <li className="li-convert-content">
           <h4>Inchs:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The inch {"("}symbol: in or ″{")"} is a unit of length in the British
+          imperial and the United States customary systems of measurement. It is
+          equal to 1 / 36 yard or 1 / 12 of a foot. Derived from the Roman uncia
+          {"("}"twelfth"{")"}, the word inch is also sometimes used to translate
+          similar units in other measurement systems, usually understood as
+          deriving from the width of the human thumb.
+        </p>
+        <p className="span-convert-content">
+          Standards for the exact length of an inch have varied in the past, but
+          since the adoption of the international yard during the 1950s and
+          1960s the inch has been based on the metric system and defined as
+          exactly 25.4 mm.
+        </p>
         <li className="li-convert-content">
           <h4>Milimeters:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The millimetre {"("}international spelling; SI unit symbol mm{")"} or
+          millimeter {"("}American spelling{")"} is a unit of length in the
+          International System of Units {"("}SI{")"}, equal to one thousandth of
+          a metre, which is the SI base unit of length. Therefore, there are one
+          thousand millimetres in a metre. There are ten millimetres in a
+          centimetre.
+        </p>
+        <p className="span-convert-content">
+          One millimetre is equal to 1000 micrometres or 1000000 nanometres.
+          Since an inch is officially defined as exactly 25.4 millimetres, a
+          millimetre is equal to exactly 5⁄127 {"("}≈ 0.03937{")"} of an inch.
+        </p>
         <li className="li-convert-content">
           <h4>Feets:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The feet {"("}pl. foot{")"}, standard symbol: ft, is a unit of length
+          in the British imperial and United States customary systems of
+          measurement. The prime symbol, ′, is commonly used to represent the
+          foot. In both customary and imperial units, one foot comprises 12
+          inches, and one yard comprises three feet. Since an international
+          agreement in 1959, the foot is defined as equal to exactly 0.3048
+          meters.
+        </p>
+        <p className="span-convert-content">
+          Historically, the "foot" was a part of many local systems of units,
+          including the Greek, Roman, Chinese, French, and English systems. It
+          varied in length from country to country, from city to city, and
+          sometimes from trade to trade. Its length was usually between 250 mm
+          and 335 mm and was generally, but not always, subdivided into 12
+          inches or 16 digits.
+        </p>
+        <p className="span-convert-content">
+          The United States is the only industrialized country that uses the
+          {"("}international{")"} foot in preference to the meter in its
+          commercial, engineering, and standards activities. The foot is legally
+          recognized in the United Kingdom; road distance signs must use
+          imperial units {"("}however, distances on road signs are always marked
+          in miles or yards, not feet; bridge clearances are given in meters as
+          well as feet and inches{")"}, while its usage is widespread among the
+          British public as a measurement of height. The foot is recognized as
+          an alternative expression of length in Canada. Both the UK and Canada
+          have partially metricated their units of measurement. The measurement
+          of altitude in international aviation
+          {"("}the flight level unit{")"} is one of the few areas where the foot
+          is used outside the English-speaking world.
+        </p>
+        <p className="span-convert-content">
+          The length of the {"("}international{")"} foot corresponds to a human
+          foot with shoe size of 13 {"("}UK{")"}, 14 {"("}US male{")"}, 15.5{" "}
+          {"("}US female{")"} or 48 {"("}EU sizing{")"}.
+        </p>
         <li className="li-convert-content">
           <h4>Meters:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The meter {"("}or metre in American spelling; symbol: m{")"} is the
+          base unit of length in the International System of Units {"("}SI{")"}.
+          Since 2019 the metre has been defined as the length of the path
+          travelled by light in vacuum during a time interval of 1 / 299792458
+          of a second, where the second is defined by a hyperfine transition
+          frequency of caesium
+        </p>
+        <p className="span-convert-content">
+          The metre was originally defined in 1791 by the French National
+          Assembly as one ten-millionth of the distance from the equator to the
+          North Pole along a great circle, so the Earth's polar circumference is
+          approximately 40000 km.
+        </p>
+        <p className="span-convert-content">
+          In 1799, the metre was redefined in terms of a prototype metre bar,
+          the bar used was changed in 1889, and in 1960 the metre was redefined
+          in terms of a certain number of wavelengths of a certain emission line
+          of krypton-86. The current definition was adopted in 1983 and modified
+          slightly in 2002 to clarify that the metre is a measure of proper
+          length. From 1983 until 2019, the metre was formally defined as the
+          length of the path travelled by light in vacuum in 1 / 299792458 of a
+          second. After the 2019 redefinition of the SI base units, this
+          definition was rephrased to include the definition of a second in
+          terms of the caesium frequency ΔνCs. This series of amendments did not
+          alter the size of the metre significantly – today Earth's polar
+          circumference measures 40007.863 km, a change of 0.022% from the
+          original value of exactly 40000 km, which also includes improvements
+          in the accuracy of measuring the circumference.
+        </p>
         <li className="li-convert-content">
           <h4>Yards:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The yard {"("}symbol: yd{")"} is an English unit of length in both the
+          British imperial and US customary systems of measurement equalling 3
+          feet or 36 inches. Since 1959 it has been by international agreement
+          standardized as exactly 0.9144 meter. A distance of 1,760 yards is
+          equal to 1 mile.
+        </p>
+        <p className="span-convert-content">
+          The US survey yard is very slightly longer.
+        </p>
         <li className="li-convert-content">
           <h4>Miles:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The mile, sometimes the international mile or statute mile to
+          distinguish it from other miles, is a British imperial unit and United
+          States customary unit of distance; both are based on the older English
+          unit of length equal to 5,280 English feet, or 1,760 yards. The
+          statute mile was standardised between the Commonwealth of Nations and
+          the United States by an international agreement in 1959, when it was
+          formally redefined with respect to SI units as exactly 1,609.344
+          metres.
+        </p>
+        <p className="span-convert-content">
+          With qualifiers, mile is also used to describe or translate a wide
+          range of units derived from or roughly equivalent to the Roman mile
+          {"("}roughly 1.48 km{")"}, such as the nautical mile {"("}now 1.852 km
+          exactly{")"}, the Italian mile {"("}roughly 1.852 km{")"}, and the
+          Chinese mile {"("}now 500 m exactly{")"}. The Romans divided their
+          mile into 5,000 pedēs {"("}"feet"{")"}, but the greater importance of
+          furlongs in the Elizabethan-era England meant that the statute mile
+          was made equivalent to 8 furlongs or 5,280 feet in 1593. This form of
+          the mile then spread across the British Empire, some successor states
+          of which continue to employ the mile. The US Geological Survey now
+          employs the metre for official purposes, but legacy data from its 1927
+          geodetic datum has meant that a separate US survey mile {"("} 6336 /
+          3937 km{")"}continues to see some use, although it was officially
+          phased out in 2022. While most countries replaced the mile with the
+          kilometre when switching to the International System of Units {"("}SI
+          {")"}, the international mile continues to be used in some countries,
+          such as Liberia, the United Kingdom, the United States, and a number
+          of countries with fewer than one million inhabitants, most of which
+          are UK or US territories or have close historical ties with the UK or
+          US.
+        </p>
         <li className="li-convert-content">
           <h4>Kilometers:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The kilometre {"("}SI symbol: km; /ˈkɪləmiːtər/ or /kɪˈlɒmətər/{")"},
+          spelt kilometer in American English and Philippine English, is a unit
+          of length in the International System of Units {"("}SI{")"}, equal to
+          one thousand metres {"("}kilo- being the SI prefix for 1000{")"}. It
+          is now the measurement unit used for expressing distances between
+          geographical places on land in most of the world; notable exceptions
+          are the United States and the United Kingdom where the statute mile is
+          the main unit used.
+        </p>
+        <p className="span-convert-content">
+          The abbreviations k or K {"("}pronounced /keɪ/{")"} are commonly used
+          to represent kilometre, but are not recommended by the BIPM. A slang
+          term for the kilometre in the US, UK, and Canadian militaries is klick
+        </p>
       </ul>
     </>
   );
--- src/components/VolumeContent.jsx
+++ src/components/VolumeContent.jsx
@@ -5,38 +5,136 @@ const VolumeContent = () => {
         <li className="li-convert-content">
           <h4>Us Gallons:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The gallon is a unit of volume in British imperial units and United
+          States customary units. Three different versions are in current use:
+        </p>
+        <ul style={{ marginLeft: "40px" }}>
+          <li className="li-convert-content">
+            the imperial gallon {"("}imp gal{")"}, defined as 4.54609 litres,
+            which is or was used in the United Kingdom, Ireland, Canada,
+            Australia, New Zealand, and some Caribbean countries;
+          </li>
+          <li className="li-convert-content">
+            the US gallon {"("}US gal{")"}, defined as 231 cubic inches {"("}
+            exactly 3.785411784 L{")"}, which is used in the United States and
+            some Latin American and Caribbean countries; and
+          </li>
+          <li className="li-convert-content">
+            the US dry gallon {"("}"usdrygal"{")"}, defined as 1⁄8 US bushel{" "}
+            {"("}exactly 4.40488377086 L{")"}.
+          </li>
+        </ul>
+        <p className="span-convert-content">
+          There are two pints in a quart and four quarts in a gallon. Different
+          sizes of pints account for the different sizes of the imperial and US
+          gallons.
+        </p>
+        <p className="span-convert-content">
+          The IEEE standard symbol for both US {"("}liquid{")"} and imperial
+          gallon is gal, not to be confused with the gal {"("}symbol: Gal{")"},
+          a CGS unit of acceleration.
+        </p>
         <li className="li-convert-content">
           <h4>Liters:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The litre {"("}international spelling{")"} or liter {"("}American
+          English spelling{")"} {"("}SI symbols L and l, other symbol used: ℓ
+          {")"} is a metric unit of volume. It is equal to 1 cubic decimetre{" "}
+          {"("}dm<sup>3</sup>
+          {")"}, 1000 cubic centimetres {"("}cm<sup>3</sup>
+          {")"} or 0.001 cubic metre {"("}m<sup>3</sup>
+          {")"}. A cubic decimetre {"("}or litre{")"} occupies a volume of 10 cm
+          × 10 cm × 10 cm {"("}see figure
+          {")"} and is thus equal to one-thousandth of a cubic metre.
+        </p>
+        <p className="span-convert-content">
+          The original French metric system used the litre as a base unit. The
+          word litre is derived from an older French unit, the litron, whose
+          name came from Byzantine Greek—where it was a unit of weight, not
+          volume—via Late Medieval Latin, and which equalled approximately 0.831
+          litres. The litre was also used in several subsequent versions of the
+          metric system and is accepted for use with the SI, although not an SI
+          unit—the SI unit of volume is the cubic metre {"("}m<sup>3</sup>
+          {")"}. The spelling used by the International Bureau of Weights and
+          Measures is "litre", a spelling which is shared by most
+          English-speaking countries. The spelling "liter" is predominantly used
+          in American English.
+        </p>
+        <p className="span-convert-content">
+          One litre of liquid water has a mass of almost exactly one kilogram,
+          because the kilogram was originally defined in 1795 as the mass of one
+          cubic decimetre of water at the temperature of melting ice {"("}0 °C
+          {")"}. Subsequent redefinitions of the metre and kilogram mean that
+          this relationship is no longer exact.
+        </p>
         <li className="li-convert-content">
           <h4>Cubic Feets:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The cubic foot {"("}symbol ft3 or cu ft{")"} is an imperial and US
+          customary {"("}non-metric{")"} unit of volume, used in the United
+          States and the United Kingdom. It is defined as the volume of a cube
+          with sides of one foot {"("}0.3048 m{")"} in length. Its volume is
+          28.3168 L {"("}about 1⁄35 of a cubic metre{")"}.
+        </p>
+
         <li className="li-convert-content">
           <h4>Cubic Meters:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          The cubic metre {"("}in Commonwealth English and international
+          spelling as used by the International Bureau of Weights and Measures
+          {")"} or cubic meter {"("}in American English{")"} is the unit of
+          volume in the International System of Units {"("}SI{")"}. Its symbol
+          is m<sup>3</sup>. It is the volume of a cube with edges one metre in
+          length. An alternative name, which allowed a different usage with
+          metric prefixes, was the stère, still sometimes used for dry measure{" "}
+          {"("}for instance, in reference to wood{")"}. Another alternative
+          name, no longer widely used, was the kilolitre.
+        </p>
+
         <li className="li-convert-content">
           <h4>Cubic Yards:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          A cubic yard {"("}symbol yd<sup>3</sup>
+          {")"} is an Imperial / U.S. customary {"("}non-SI non-metric{")"} unit
+          of volume, used in Canada and the United States. It is defined as the
+          volume of a cube with sides of 1 yard {"("}3 feet, 36 inches, 0.9144
+          meters{")"} in length.
+        </p>
+
         <li className="li-convert-content">
           <h4>US Fluid Ounces:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          A fluid ounce {"("}abbreviated fl oz, fl. oz. or oz. fl., old forms ℥,
+          fl ℥, f℥, ƒ ℥ {")"} is a unit of volume {"("}also called capacity{")"}{" "}
+          typically used for measuring liquids. The British Imperial, the United
+          States customary, and the United States food labeling fluid ounce are
+          the only three that are still in common use, although various
+          definitions have been used throughout history.
+        </p>
+        <p className="span-convert-content">
+          An imperial fluid ounce is 1⁄20 of an imperial pint, 1⁄160 of an
+          imperial gallon or exactly 28.4130625 mL
+        </p>
+        <p className="span-convert-content">
+          A US customary fluid ounce is 1⁄16 of a US liquid pint and 1⁄128 of a
+          US liquid gallon or exactly 29.5735295625 mL, making it about 4.08%
+          larger than the imperial fluid ounce.
+        </p>
+        <p className="span-convert-content">
+          A US food labeling fluid ounce is exactly 30 mL.
+        </p>
         <li className="li-convert-content">
           <h4>Mililiters:</h4>
         </li>
-        <p className="span-convert-content"></p>
-        <p className="span-convert-content"></p>
+        <p className="span-convert-content">
+          a unit of measurement of volume equal to 0.001 liter
+        </p>
       </ul>
     </>
   );
