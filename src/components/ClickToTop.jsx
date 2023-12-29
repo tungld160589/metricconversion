ba7fca971e4a8e8e19572d94a848b0b1db517b26
tungld160589 <148056136+tungld160589@users.noreply.github.com>
2023-12-29 08:20:44 +0700

    cập nhật css Cho click to top

--- src/components/ClickToTop.jsx
+++ src/components/ClickToTop.jsx
@@ -2,49 +2,33 @@ import { useState, useEffect } from "react";
 import React from "react";
 
 const ClickToTop = () => {
-  // Function to handle the button click
   const [visible, setVisible] = useState(false);
-  // Function to handle the button click
+
   const handleButtonClick = () => {
     window.scrollTo({ top: 0, behavior: "smooth" });
   };
 
-  // Function to handle scroll events
   const handleScroll = () => {
-    // Show/hide the button based on the scroll position
     const scrollY = window.scrollY;
-    const threshold = 100; // Adjust this value based on your preference
-
+    const threshold = 100;
     setVisible(scrollY > threshold);
   };
 
-  // Attach scroll event listener on mount
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
-
-    // Cleanup the event listener on component unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
   return (
-    <div>
-      {/* <div
-        className="back-to-top-group"
-        style={{ display: visible ? "block" : "none" }}
-      >
-        <span></span>
-        
-        <span className="back-to-top-text">Back to top</span>
-      </div> */}
-      <button
-        onClick={handleButtonClick}
-        className="back-to-top-group"
-        style={{ display: visible ? "block" : "none" }}
-      >
-        &and;
-      </button>
-      ;
+    <div
+      className="back-to-top-group"
+      style={{ display: visible ? "block" : "none" }}
+      onClick={handleButtonClick}
+    >
+      <button className="back-to-top-btn">&and;</button>
+      <p className="txt-back-to-top">Back to Top</p>
     </div>
   );
 };
