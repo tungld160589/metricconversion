2221b89aedbd949768b9f1cf7215564e76992ab8
NguyenLK8890 <nguyenlekhanh8890@gmail.com>
2023-12-29 10:12:02 +0700

    Update Art_USMetriction.jsx

--- src/pages/Art_USMetriction.jsx
+++ src/pages/Art_USMetriction.jsx
@@ -12,7 +12,7 @@ const Art_USMetriction = () => {
       </div>
       <div className="Art">
         <p>Article-U.S Metriction</p>
-        <input className="Art-Search" type="text" placeholder="Enter Article" />
+        {/* <input className="Art-Search" type="text" placeholder="Enter Article" /> */}
       </div>
       <div className="Art-Unit">
         <div>
