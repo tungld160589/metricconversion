d0e061963d85f5c1d73a7d33b77a7d3c6e753218
NguyenLK8890 <nguyenlekhanh8890@gmail.com>
2023-12-29 15:56:12 +0700

    Update Art_USMetriction.jsx

--- src/pages/Art_USMetriction.jsx
+++ src/pages/Art_USMetriction.jsx
@@ -24,63 +24,63 @@ const Art_USMetriction = () => {
       </div>
       <div className="Us_In">
         <h5>
-          The United States, like many other countries, has a rich history of
-          traditional units of measurement that were used for centuries before
-          the adoption of the metric system. While the metric system is widely
-          recognized and utilized in scientific and international contexts,
-          traditional units of measurement still hold a place in American
-          culture and everyday life. This article explores some of the
-          traditional units that have played a significant role in the United
-          States.
+          <p className="US1"> The United States, like many other countries, has a rich history of
+            traditional units of measurement that were used for centuries before
+            the adoption of the metric system. While the metric system is widely
+            recognized and utilized in scientific and international contexts,
+            traditional units of measurement still hold a place in American
+            culture and everyday life. This article explores some of the
+            traditional units that have played a significant role in the United
+            States.</p>
         </h5>
       </div>
       <div className="Us">
         <h3>Length Measurement:</h3>
         <br />
         <h5>1. Inches, Feet, and Yards:</h5>
-        Derived from the English system, these units are commonly used for
-        measuring lengths in everyday life. Twelve inches make a foot, and three
-        feet make a yard. These units are prevalent in construction, carpentry,
-        and home improvement projects.br <br />
+        <p className="US1">Derived from the English system, these units are commonly used for
+          measuring lengths in everyday life. Twelve inches make a foot, and three
+          feet make a yard. These units are prevalent in construction, carpentry,
+          and home improvement projects.</p> <br />
         <h5>2. Miles:</h5>
-        Miles are used to measure longer distances, especially in the context of
-        road travel. The mile is still the standard unit for expressing
-        distances on road signs and maps across the United States. <br />
+        <p className="US1">Miles are used to measure longer distances, especially in the context of
+          road travel. The mile is still the standard unit for expressing
+          distances on road signs and maps across the United States.</p> <br />
         <h5>3. Surveyor's Units:</h5>
-        In land surveying, the chain (66 feet), rod (16.5 feet or 1/4 chain),
-        and acre (43,560 square feet) are traditional units that have historical
-        roots. These units are often encountered in property descriptions and
-        legal documents.
+        <p className="US1">In land surveying, the chain (66 feet), rod (16.5 feet or 1/4 chain),
+          and acre (43,560 square feet) are traditional units that have historical
+          roots. These units are often encountered in property descriptions and legal documents.</p>
+
       </div>
       <div className="Us">
         <h3>Weight and Mass Measurements:</h3>
         <br />
         <h5>1.Pounds and Ounces:</h5>
-        Pounds and ounces are widely used for measuring weight in the United
-        States. From grocery store purchases to body weight, these units remain
-        ingrained in daily life. <br />
+        <p className="US1">Pounds and ounces are widely used for measuring weight in the United
+          States. From grocery store purchases to body weight, these units remain
+          ingrained in daily life.</p> <br />
         <h5>2.Tons:</h5>
-        Tons are used to measure larger quantities of weight, particularly in
-        industries like shipping, construction, and mining.
+        <p className="US1">Tons are used to measure larger quantities of weight, particularly in
+          industries like shipping, construction, and mining.</p>
       </div>
       <div className="Us">
         <h3>Temperature:</h3>
         <br />
         <h5>1.Fahrenheit:</h5>
-        The Fahrenheit scale is the standard unit of temperature in the United
-        States for weather forecasts, household thermometers, and cooking. It
-        differs from the Celsius scale used in the metric system.
+        <p className="US1">The Fahrenheit scale is the standard unit of temperature in the United
+          States for weather forecasts, household thermometers, and cooking. It
+          differs from the Celsius scale used in the metric system.</p>
       </div>
       <div className="Us">
         <h3>Legacy and Everyday Use</h3>
         <br />
-        While the metric system is the standard in many scientific and global
-        contexts, traditional units continue to be a part of everyday American
-        life. From cooking recipes and road signs to conversations about height
-        and weight, these units persist in the cultural fabric of the United
-        States. Understanding both traditional and metric systems is essential
-        for effective communication and navigating diverse measurement practices
-        in various contexts.
+        <p className="US1">While the metric system is the standard in many scientific and global
+          contexts, traditional units continue to be a part of everyday American
+          life. From cooking recipes and road signs to conversations about height
+          and weight, these units persist in the cultural fabric of the United
+          States. Understanding both traditional and metric systems is essential
+          for effective communication and navigating diverse measurement practices
+          in various contexts.</p>
       </div>
       <div className="btnback">
         <Link to={"/Article"}>
