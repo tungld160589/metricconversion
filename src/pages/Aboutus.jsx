e0e32fc57e54f16a5d029c17e41933e412e1d6ca
tungld160589 <148056136+tungld160589@users.noreply.github.com>
2024-01-04 18:26:16 +0700

    cập nhật aboutus và logo

--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -17,14 +17,15 @@ const Aboutus = () => {
         <Nav />
       </div>
       <div className="aboutus-container">
-        <div>
-          <p className="txt-aboutus-header"> ABOUT US</p>
+        <div className="txt-aboutus-header">
+          <p> ABOUT</p>
+          <p> US</p>
         </div>
         <div className="aboutus-left-group">
           <div className="aboutus-content-group aboutus-odd">
             <div className="txt-aboutus-group ">
               <p className="txt-title-content-aboutus">Who We Are: </p>
-              <p>
+              <p style={{ fontSize: 16, fontStyle: "italic" }}>
                 We are a dynamic group of Information Technology students
                 passionate about simplifying everyday tasks through innovative
                 solutions. Our journey began with a shared enthusiasm for
@@ -42,7 +43,7 @@ const Aboutus = () => {
             </div>
             <div className="txt-aboutus-group aboutus-even">
               <p className="txt-title-content-aboutus">Our Mission: </p>
-              <p>
+              <p style={{ fontSize: 16, fontStyle: "italic" }}>
                 At UnitConverter, our mission is to provide a user-friendly
                 platform that facilitates quick and accurate unit conversions.
                 We understand the importance of efficiency in today's fast-paced
@@ -54,35 +55,34 @@ const Aboutus = () => {
           <div className="aboutus-content-group aboutus-odd">
             <div className="txt-aboutus-group ">
               <div className="txt-aboutus-group ">
-                <p className="txt-title-content-aboutus">
-                  What Sets Us Apart:{" "}
-                </p>
-
-                <p style={{ marginBottom: 10 }}>
-                  <span className="txt-aboutus-strong">Innovation: </span>
-                  We constantly strive to incorporate the latest technologies
-                  and methodologies to enhance the user experience. Our
-                  commitment to innovation drives us to stay at the forefront of
-                  the ever-evolving tech landscape.
-                </p>
-
-                <p style={{ marginBottom: 10 }}>
-                  <span className="txt-aboutus-strong">
-                    User-Centric Design:{" "}
-                  </span>
-                  Our team places a strong emphasis on user-centric design. We
-                  believe that technology should be intuitive and user-friendly,
-                  ensuring that our platform is accessible to individuals with
-                  varying levels of technical expertise.
-                </p>
-
-                <p style={{ marginBottom: 10 }}>
-                  <span className="txt-aboutus-strong">Collaboration: </span>
-                  Group04 operates on the principles of collaboration and
-                  teamwork. Each member brings a unique set of skills and
-                  perspectives, fostering an environment where creativity
-                  thrives and solutions flourish.
-                </p>
+                <p className="txt-title-content-aboutus">What Sets Us Apart:</p>
+                <div className="txt-wsua-content-aboutus">
+                  <p>Innovation:</p>
+                  <p>
+                    {"  "}We constantly strive to incorporate the latest
+                    technologies and methodologies to enhance the user
+                    experience. Our commitment to innovation drives us to stay
+                    at the forefront of the ever-evolving tech landscape.
+                  </p>
+                </div>
+                <div className="txt-wsua-content-aboutus">
+                  <p>User-Centric Design:</p>
+                  <p>
+                    {"  "}Our team places a strong emphasis on user-centric
+                    design. We believe that technology should be intuitive and
+                    user-friendly, ensuring that our platform is accessible to
+                    individuals with varying levels of technical expertise.
+                  </p>
+                </div>
+                <div className="txt-wsua-content-aboutus">
+                  <p>Collaboration:</p>
+                  <p>
+                    {"  "}Group04 operates on the principles of collaboration
+                    and teamwork. Each member brings a unique set of skills and
+                    perspectives, fostering an environment where creativity
+                    thrives and solutions flourish.
+                  </p>
+                </div>
               </div>
             </div>
             <div className="aboutus-img-left">
@@ -93,19 +93,20 @@ const Aboutus = () => {
       </div>
       <div className="group-session">
         <div className="aboutus-profile-container">
-          <p className="txt-aboutus-head-profile">MEET THE TEAM</p>
+          <p className="txt-aboutus-head-profile">Meet The Team</p>
           <div className="aboutus-profilegroup">
             <div className="aboutus-profile-txt-group">
-              <p className="txt-title-profile-aboutus">
-                Lượng Đức Tùng - Project Manager/Lead Developer/UX/UI Designer:
+              <p className="txt-title-profile-aboutus">Lượng Đức Tùng</p>
+              <p className="job-profile-aboutus">
+                Project Manager/Lead Developer/UX/UI Designer
               </p>
+              <hr className="hr-profile-aboutus" />
               <ul className="li-aboutus">
-                <li className="li-aboutus">
-                  Design: HomePage,ConvertsPage,Aboutus
+                <li className="li-aboutus role-profile">
+                  Design: HomePage, convertsion pages, aboutus
                 </li>
-                <li className="li-aboutus">
-                  Developer : conversions content, conversion pages, nav,
-                  Homepage, Aboutus
+                <li className="li-aboutus role-profile">
+                  Developer : Conversion pages, Homepage, Aboutus
                 </li>
               </ul>
             </div>
@@ -118,16 +119,16 @@ const Aboutus = () => {
               <img className="img-profile" src={nguyenimg} alt="" />
             </div>
             <div className="aboutus-profile-txt-group">
-              <p className="txt-title-profile-aboutus">
-                Lê Khánh Nguyên - Developer/UX/UI Designer:
-              </p>
+              <p className="txt-title-profile-aboutus">Lê Khánh Nguyên</p>
+              <p className="job-profile-aboutus">Developer/UX/UI Designer</p>
+              <hr className="hr-profile-aboutus" />
               <ul className="li-aboutus">
-                <li className="li-aboutus">
-                  Design: Artice page and page for artiles
+                <li className="li-aboutus role-profile">
+                  Design: Article page and page for artiles
                 </li>
-                <li className="li-aboutus">
-                  Developer : Artice page and page for artiles, search component
-                  for article
+                <li className="li-aboutus role-profile">
+                  Developer : Article page and page for artiles, search
+                  component for article
                 </li>
               </ul>
             </div>
@@ -136,34 +137,28 @@ const Aboutus = () => {
         <div className="aboutus-form-group">
           <div className="txt-aboutus-head-profile">Contact Us</div>
           <div className="aboutus-form">
-            <label>First Name: </label>
+            <label className="label-form-contactus">First Name: </label>
             <input
               className="aboutus-form-input"
               type="text"
-              name=""
-              id=""
               placeholder="Enter First Name"
             />
 
-            <label>Last Name: </label>
+            <label className="label-form-contactus">Last Name: </label>
             <input
               className="aboutus-form-input"
               type="text"
-              name=""
-              id=""
               placeholder="Enter Last Name"
             />
 
-            <label>Your Email: </label>
+            <label className="label-form-contactus">Your Email: </label>
             <input
               className="aboutus-form-input"
               type="email"
-              name="txtemail"
-              id="txtemail"
               placeholder="Enter Your Email Address"
             />
 
-            <label>Content: </label>
+            <label className="label-form-contactus">Content: </label>
             <textarea name="" id="" cols="30" rows="10"></textarea>
             <div className="btn-control-aboutus">
               <button className="aboutus-form-btn">Send</button>
