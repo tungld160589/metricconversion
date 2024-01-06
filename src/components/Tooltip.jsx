57e4f8840ae5c9949eadbdb304223fbd435231de
tungld160589 <148056136+tungld160589@users.noreply.github.com>
2024-01-06 22:51:51 +0700

    cập nhật form contactus

---
+++ src/components/Tooltip.jsx
@@ -0,0 +1,15 @@
+const Tooltip = (props) => {
+  const { text, textarea } = props;
+  return (
+    <div className="bottom" style={textarea ? { top: 55 } : { top: 35 }}>
+      <div className="bottom-tooltip-group">
+        <p className="tooltip-alert">&#x26A0;</p>
+        <p>{text}</p>
+      </div>
+
+      <i></i>
+    </div>
+  );
+};
+
+export default Tooltip;
--- src/pages/Aboutus.jsx
+++ src/pages/Aboutus.jsx
@@ -1,4 +1,4 @@
-import Nav from "../components/Nav";
+import NavBar from "../components/Nav";
 import Footer from "../components/Footer";
 import ClickToTop from "../components/ClickToTop";
 import Imgteam from "../img/Team-img2.jpg";
@@ -6,25 +6,94 @@ import Outmission from "../img/OutMission.jpg";
 import WhatSetsUsApart from "../img/WhatSetsUsApart.jpg";
 import tungjpg from "../img/Tung.jpg";
 import nguyenimg from "../img/Nguyen-img.jpg";
+import React from "react";
 import { useEffect, useState } from "react";
+import Tooltip from "../components/Tooltip";
+import { useNavigate } from "react-router-dom";
 
 const Aboutus = () => {
-  const [fName, setFName] = useState();
-  const [lName, setLName] = useState();
-  const [yEmail, setYEmail] = useState();
-  const [txtSumbit, setTxtSubmit] = useState();
-  const [errorFName, setErrorFName] = useState(false);
-  const [errorLName, setErrorLName] = useState(false);
+  const [fName, setFName] = useState("");
+  const [lName, setLName] = useState("");
+  const [yEmail, setYEmail] = useState("");
+  const [content, setContent] = useState("");
+  const [errors, setErrors] = useState({});
+  const [errorFName, setErrorFname] = useState(false);
+  const [errorLName, setErrorLname] = useState(false);
+  const [errorContent, setErrorContent] = useState(false);
   const [errorYEmail, setErrorYEmail] = useState(false);
-  const [errorTxtSubmit, setErrorTxtSubmit] = useState(false);
+  const navigate = useNavigate();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  // Xử lý sự kiện submit của form
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // Kiểm tra và cập nhật trạng thái lỗi
+    const formErrors = validateForm();
+    setErrors(formErrors);
+
+    // Nếu không có lỗi, thực hiện logic submit
+    if (Object.keys(formErrors).length === 0) {
+      // Thực hiện logic submit ở đây
+      alert(`Thank you: ${fName} ${lName} with Email: ${yEmail}
+    for your content submit: ${content} .
+    We will contact you soon!
+    `);
+      navigate("/");
+    }
+  };
+
+  const validateForm = () => {
+    const inputFName = document.getElementById("fName");
+    const inputLName = document.getElementById("lName");
+    const inputYMail = document.getElementById("yEmail");
+    const inputContent = document.getElementById("content");
+    const reName =
+      /^\w[a-zA-ZàáâäãåąčćęèéêëėįìíîïłńòóôöõøùúûüųūÿýżźñçčšžÀÁÂÄÃÅĄĆČĖĘÈÉÊËÌÍÎÏĮŁŃÒÓÔÖÕØÙÚÛÜŲŪŸÝŻŹÑßÇŒÆČŠŽ∂ð ,.'-]+$/u;
+    const reMail =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    const errors = {};
+
+    if (!fName.trim()) {
+      errors.fName = "First Name is required";
+      inputFName.focus();
+      setErrorFname(!errorFName);
+    } else if (!reName.test(fName)) {
+      errors.fName = "First name is not letters";
+      inputFName.focus();
+      setErrorFname(!errorFName);
+    } else if (!lName.trim()) {
+      errors.lName = "Last Name is required";
+      inputLName.focus();
+      setErrorLname(!errorLName);
+    } else if (!reName.test(lName)) {
+      errors.lName = "Last name is not letters";
+      inputLName.focus();
+      setErrorLname(!errorLName);
+    } else if (!yEmail.trim()) {
+      errors.yEmail = "Email is required";
+      inputYMail.focus();
+      setErrorYEmail(!errorYEmail);
+    } else if (!reMail.test(yEmail)) {
+      errors.yEmail = "Email address Invalid";
+      inputYMail.focus();
+      setErrorYEmail(!errorYEmail);
+    } else if (!content.trim()) {
+      errors.content = "Content is required";
+      inputContent.focus();
+      setErrorContent(!errorContent);
+    }
+
+    return errors;
+  };
+
   return (
     <>
       <div>
-        <Nav />
+        <NavBar />
       </div>
       <div className="aboutus-container">
         <div className="txt-aboutus-header">
@@ -146,48 +215,82 @@ const Aboutus = () => {
         </div>
         <div className="aboutus-form-group">
           <div className="txt-aboutus-head-profile">Contact Us</div>
-          <div className="aboutus-form">
+          <form className="aboutus-form" onSubmit={handleSubmit}>
             <label className="label-form-contactus">First Name: </label>
-            <input
-              className="aboutus-form-input"
-              type="text"
-              value={fName}
-              placeholder="Enter First Name"
-              onChange={(e) => setFName(e.target.value)}
-            />
-            <div className="error-div-form-aboutus">Error</div>
+            <div className="tooltip">
+              <input
+                id="fName"
+                type="text"
+                className="aboutus-form-input"
+                value={fName}
+                placeholder="Enter First Name"
+                onChange={(e) => {
+                  setFName(e.target.value);
+                  {
+                    errorFName && setErrorFname(!errorFName);
+                  }
+                }}
+              />
+              {errorFName && <Tooltip text={errors.fName} />}
+            </div>
             <label className="label-form-contactus">Last Name: </label>
-            <input
-              className="aboutus-form-input"
-              type="text"
-              value={lName}
-              placeholder="Enter Last Name"
-              onChange={(e) => setLName(e.target.value)}
-            />
-            <div className="error-div-form-aboutus">Error</div>
+            <div className="tooltip">
+              <input
+                className="aboutus-form-input"
+                id="lName"
+                type="text"
+                value={lName}
+                placeholder="Enter Last Name"
+                onChange={(e) => {
+                  setLName(e.target.value);
+                  errorLName && setErrorLname(!errorLName);
+                }}
+              />
+              {errorLName && <Tooltip text={errors.lName} />}
+            </div>
+
             <label className="label-form-contactus">Your Email: </label>
-            <input
-              className="aboutus-form-input"
-              type="email"
-              value={yEmail}
-              placeholder="Enter Your Email Address"
-              onChange={(e) => setYEmail(e.target.value)}
-            />
-            <div className="error-div-form-aboutus">Error</div>
+            <div className="tooltip">
+              <input
+                className="aboutus-form-input"
+                id="yEmail"
+                type="txt"
+                value={yEmail}
+                placeholder="Enter Your Email Address"
+                onChange={(e) => {
+                  setYEmail(e.target.value);
+                  errorYEmail && setErrorYEmail(!errorYEmail);
+                }}
+              />
+              {errorYEmail && <Tooltip text={errors.yEmail} />}
+            </div>
+
             <label className="label-form-contactus">Content: </label>
-            <textarea
-              value={txtSumbit}
-              cols="30"
-              rows="10"
-              onChange={(e) => setTxtSubmit(e.target.value)}
-            ></textarea>
-            <div className="error-div-form-aboutus">Error</div>
+            <div className="tooltip">
+              <textarea
+                id="content"
+                value={content}
+                cols="35"
+                rows="10"
+                onChange={(e) => {
+                  setContent(e.target.value);
+                  {
+                    errorContent && setErrorContent(!errorContent);
+                  }
+                }}
+              />
+              {errorContent && (
+                <Tooltip text={errors.content} textarea={true} />
+              )}
+            </div>
+
             <div className="btn-control-aboutus">
               <button className="aboutus-form-btn">Send</button>
             </div>
-          </div>
+          </form>
         </div>
       </div>
+
       <div>
         <Footer />
       </div>
